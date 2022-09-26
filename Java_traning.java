b95b92db6af42b1d2bad0dff5824903ea3a38d99
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-09-27 00:17:29 +0200

    variable

--- Java_traning.java
+++ Java_traning.java
@@ -1,5 +1,21 @@
-class Hello {
+/**
+ class Hello {
     public static void main(String[] args) {
         System.out.println("Bonjour Java !!!");
+    }
+}
+ */
+
+/** Declaration de variable */
+
+class Training {
+    public static void main(String[] args) {
+        int agePersonne = 18;
+        agePersonne = 33;
+
+        agePersonne = agePersonne + 20;
+
+        System.out.println("Age = " + agePersonne + " ans");
+
     }
 }
