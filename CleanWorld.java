4336bfe2dd0e3b1a81118a6bd4d0ea2462ee8715
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-09-29 01:09:30 +0200

    function Java

---
+++ CleanWorld.java
@@ -0,0 +1,10 @@
+
+public class CleanWorld {
+    public static void main(String[] args) {
+        sayHelloTo("world");
+    }
+
+    private static void sayHelloTo(String recipient) {
+        System.out.println("Hello" + recipient);
+    }
+}
---
+++ java_training.java
@@ -0,0 +1,11 @@
+public class java_training {
+    public static void main(String[] args) {
+
+        String movie = "Best ever";
+
+        movie = movie.replace("Best ever", "Ayoka");
+        movie = movie.toUpperCase();
+
+        System.out.println(movie);
+    }
+}
