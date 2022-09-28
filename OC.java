60c02fee0b86be7f0de2fc60038a2bcd8084654c
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-09-29 01:27:05 +0200

    Function

---
+++ OC.java
@@ -0,0 +1,18 @@
+public class OC {
+    static String exemple = "Hello";
+
+    public static void main(String[] args) {
+        function1(args);
+        function2(args);
+
+    }
+
+    public static void function1(String[] args) {
+
+        System.out.println(exemple);
+    }
+
+    public static void function2(String[] args) {
+        System.out.println(exemple);
+    }
+}
