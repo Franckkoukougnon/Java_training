794066c3fab46a026a05281c5b5ae8794bd5dee1
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-10-01 14:33:30 +0200

    if...else

---
+++ hello.java
@@ -0,0 +1,16 @@
+public class hello {
+    public static void main(String[] args) {
+        // Declarez une variable represantant l'age
+        int age = 5;
+
+        // La condition à verifier est "age > 20"
+        if (age > 20) {
+
+            System.out.println("tu as " + age + " ans. Tu es donc majeur");
+
+        } else {
+            System.out.println("tu as " + age + " ans. Tu es donc mineur");
+        }
+
+    }
+}
