db3c0762a116cda419d2c7171376e0b58fadded8
franckkoukougnon <franckkoukougnon@yahoo.fr>
2022-10-02 22:22:31 +0200

    switch case

--- comparaison.java
+++ comparaison.java
@@ -1,20 +1,21 @@
 public class comparaison {
     public static void main(String[] args) {
         int nombre = 2;
-        if (nombre == 1) {
-            console("If 1");
-        } else if (nombre == 2) {
-            console("if 2");
-        } else {
-            console("else");
-        }
 
         switch (nombre) {
             case 1:
                 console("Switch 1");
                 break;
+
+            case 2:
+                console("Switch 2");
+                break;
+            case 3:
+                console("Switch 3");
+                break;
             default:
-                console("default");
+                console("Switch Default");
+
         }
 
     }
